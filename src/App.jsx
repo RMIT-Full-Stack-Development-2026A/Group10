6ea10e38233cfe6ba691c2f4842410631feeffe7
HoangHyunmai <118871288+HoangNguyen1904@users.noreply.github.com>
2026-05-12 21:07:57 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 // src/App.jsx
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage'; // Import trang Đăng ký vào
+import RegisterPage from './pages/RegisterPage';
+import ProfilePage from './pages/ProfilePage';
+import GamePage from './pages/GamePage'; // 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} /> {/* Thêm route này */}
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/game" element={<GamePage />} /> 
       </Routes>
     </BrowserRouter>
   );
